fe31cec097f72ac0d6eb5d11cbf60f26bd6d9090
Arlodotexe <chuck.fecht@outlook.com>
2019-05-11 15:25:10 -0500

    Fix settings check, added Legere protocol

--- core/libs.js
+++ core/libs.js
@@ -14,7 +14,7 @@ import { settings } from './helpers/settings.js';
 export function checkLib(uri, tabId, bypass) {
     let platformName;
     for (const name of Object.keys(platforms)) {
-        if ((settings[name].isEnabled || bypass) && platforms[name].baseUrlMatch(uri)) {
+        if ((settings.platforms[name].isEnabled || bypass) && platforms[name].baseUrlMatch(uri)) {
             platformName = name;
         }
     }
@@ -23,10 +23,10 @@ export function checkLib(uri, tabId, bypass) {
     // perform URL parsing for that platform's preffered client
     let protocol;
     Object.entries(platforms[platformName].clients).forEach(([clientName, client]) => {
-        if (clientName == settings[platformName].prefferedApp) {
+        if (clientName == settings.platforms[platformName].prefferedApp) {
             protocol = client.parseUrl(uri, tabId);
             setTimeout(() => {
-                if (settings[platformName].closeOnSwitch && platforms[platformName].shouldCloseOnSwitch(uri) && protocol) {
+                if (settings.platforms[platformName].closeOnSwitch && platforms[platformName].shouldCloseOnSwitch(uri) && protocol) {
                     chrome.tabs.remove(tabId);
                 }
             }, 500);
