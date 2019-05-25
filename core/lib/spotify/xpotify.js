c83893bb1af1e5051d2925dc552ad0ca4129d407
Arlodotexe <chuck.fecht@outlook.com>
2019-05-25 16:49:26 -0500

    Adding xpotify support

---
+++ core/lib/spotify/xpotify.js
@@ -0,0 +1,9 @@
+export default {
+    config: {
+        logo: "https://store-images.s-microsoft.com/image/apps.57751.13898298321492428.c8ae8ea7-9635-4987-a1cc-fb1336de0cce.0e61f8ff-c087-449f-a8d9-2113895e2417?mode=scale&q=90&h=270&w=270&background=transparent",
+        icon: "https://store-images.s-microsoft.com/image/apps.57751.13898298321492428.c8ae8ea7-9635-4987-a1cc-fb1336de0cce.0e61f8ff-c087-449f-a8d9-2113895e2417?mode=scale&q=90&h=48&w=48&background=transparent",
+        color: "#FF4500"
+    },
+    name: "Xpotify",
+    parseUrl: url => "xpotify:" + url
+}
