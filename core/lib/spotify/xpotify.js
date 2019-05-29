c152bbbe01e2ce275ff95925f7da6d438f3fe337
Arlodotexe <chuck.fecht@outlook.com>
2019-05-29 04:09:13 -0500

    Updating Xpotify logo & icons

--- core/lib/spotify/xpotify.js
+++ core/lib/spotify/xpotify.js
@@ -1,7 +1,7 @@
 export default {
     config: {
-        logo: "https://store-images.s-microsoft.com/image/apps.57751.13898298321492428.c8ae8ea7-9635-4987-a1cc-fb1336de0cce.0e61f8ff-c087-449f-a8d9-2113895e2417?mode=scale&q=90&h=270&w=270&background=transparent",
-        icon: "https://store-images.s-microsoft.com/image/apps.57751.13898298321492428.c8ae8ea7-9635-4987-a1cc-fb1336de0cce.0e61f8ff-c087-449f-a8d9-2113895e2417?mode=scale&q=90&h=48&w=48&background=transparent",
+        logo: "https://arlo.site/projects/UWPCompanion/logos/clients/Xpotify.png",
+        icon: "https://arlo.site/projects/UWPCompanion/icons/clients/Xpotify.png",
         color: "#FF4500"
     },
     name: "Xpotify",
