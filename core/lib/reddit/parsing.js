865c851c2bb1526aee320297c0e193091648f5ae
Sergio Pedri <sergio0694@live.com>
2019-07-22 19:05:21 +0200

    Added support for old.reddit.com parsing

--- core/lib/reddit/parsing.js
+++ core/lib/reddit/parsing.js
@@ -3,7 +3,7 @@
  * @returns {string} Subreddit name, if any
  */
 function isSubreddit(url) {
-    let match = url.match(/^https?:\/\/(?:www\.)?reddit\.[a-z]{0,4}\/(r\/[a-zA-Z0-9]+)\/?(.*)/);
+    let match = url.match(/^https?:\/\/(?:www\.)?(?:old\.)?reddit\.[a-z]{0,4}\/(r\/[a-zA-Z0-9]+)\/?(.*)/);
     return (match && match[2] == "" && match[1]) ? match[1] : undefined;
 }
 
@@ -12,7 +12,7 @@ function isSubreddit(url) {
  * @returns {string} Username, if any
  */
 function isUser(url) {
-    let match = url.match(/^https?:\/\/(?:www\.)?reddit\.[a-z]{0,4}\/user\/([a-zA-Z0-9]+)\/?(.*)/);
+    let match = url.match(/^https?:\/\/(?:www\.)?(?:old\.)?reddit\.[a-z]{0,4}\/user\/([a-zA-Z0-9]+)\/?(.*)/);
     return (match && match[1]) ? match[1] : undefined;
 }
 
@@ -21,7 +21,7 @@ function isUser(url) {
  * @returns {string} Post id, if any 
  */
 function isPost(url) {
-    let match = url.match(/^https?:\/\/(?:www\.)?reddit\.[a-z]{0,4}\/r\/[a-zA-Z0-9]+\/comments\/([a-zA-Z0-9]+)\/?/);
+    let match = url.match(/^https?:\/\/(?:www\.)?(?:old\.)?reddit\.[a-z]{0,4}\/r\/[a-zA-Z0-9]+\/comments\/([a-zA-Z0-9]+)\/?/);
     return (match && match[1]) ? match[1] : undefined;
 }
 
@@ -39,7 +39,7 @@ function isComment(url) {
  * @returns {boolean} Boolean
  */
 function isReddit(url) {
-    let match = url.match(/^https?:\/\/(?:www\.)?(reddit\.[a-z]{0,4})/);
+    let match = url.match(/^https?:\/\/(?:www\.)?(?:old\.)?(reddit\.[a-z]{0,4})/);
     return (match && match[1]) ? true : false;
 }
 
@@ -48,7 +48,7 @@ function isReddit(url) {
  * @returns Array containing [username, subreddit] 
  */
 function isMultireddit(url) {
-    let match = url.match(/^https?:\/\/(?:www\.)?reddit\.[a-z]{0,4}\/user\/([a-zA-Z0-9]+)\/?\/m\/(.*)/);
+    let match = url.match(/^https?:\/\/(?:www\.)?(?:old\.)?reddit\.[a-z]{0,4}\/user\/([a-zA-Z0-9]+)\/?\/m\/(.*)/);
     return (match && match[1] && match[2]) ? [match[1], match[2]] : undefined;
 }
 
