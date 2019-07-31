23e57281ee66980147c11dab977be4826a9cd570
Hanzalah Adalan <hanzalahstayzero@outlook.com>
2019-07-31 20:05:14 +0800

    Add Youtubeast 2nd Try

--- core/lib/youtube/master.js
+++ core/lib/youtube/master.js
@@ -1,10 +1,12 @@
 import myTube from './mytube.js';
+import Youtubeast from './youtubeast.js'
 import YTParser from './parsing.js';
 
 export default {
     name: "YouTube",
     baseUrlMatch: YTParser.isYoutube,
     clients: {
-        myTube
+        myTube,
+        Youtubeast
     }
 };
