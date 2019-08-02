b8d1cbefaaee0e1ea64f404f7d05f92d0e4b0175
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-02 02:22:41 -0500

    Cleaning up unsupported features

--- core/lib/youtube/youtubeast.js
+++ core/lib/youtube/youtubeast.js
@@ -3,39 +3,14 @@ import YTParser from './parsing.js';
 import { pauseVideo } from './helpers.js';
 
 function urlToProtocolRaw(url) {
-    if (YTParser.isYoutube(url)) {
-
-        if (YTParser.hasPlaylist(url) !== null) { // Is a playlist
-            if (YTParser.hasVideo(url) !== null) { // Is a playlist with a video
-                if (YTParser.hasTimestamp(url) !== null) { // Is a playlist with a video and a timestamp
-                    console.info('Playlist, video and timestamp detected. Will use protocol: ');
-                    return `youtu.beast:PlayVideo?ID=${YTParser.hasVideo(url)}&PlaylistID=${YTParser.hasPlaylist(url)}&Position=${YTParser.hasTimestamp(url)}`;
-                } else {
-                    console.info('Playlist and video detected. Will use protocol: ');
-                    return `youtu.beast:PlayVideo?ID=${YTParser.hasVideo(url)}&PlaylistID=${YTParser.hasPlaylist(url)}`;
-                }
-            } else { // Is just a playlist with no video
-                console.info('Playlist detected. Will use protocol: ');
-                return `youtu.beast:Playlist?ID=${YTParser.hasPlaylist(url)}`;
-            }
-        } else if (YTParser.hasVideo(url) !== null) { // Is a video
-            if (YTParser.hasTimestamp(url) !== null) { // Is a video with a timestamp
-                console.info('Video and timestamp detected. Will use protocol:');
-                return `youtu.beast:PlayVideo?ID=${YTParser.hasVideo(url)}&Position=${YTParser.hasTimestamp(url)}`;
-            } else {
-                console.info('Video detected. Will use protocol: \n ');
-                return `youtu.beast:PlayVideo?ID=${YTParser.hasVideo(url)}`;
-            }
-        } else if (YTParser.hasChannel(url) !== null) { // Is a channel
-            console.info('Channel detected. Will use protocol: ');
-            return `youtu.beast:Channel?ID=${YTParser.hasChannel(url)}`;
-        }
+    if (YTParser.hasVideo(url) !== null) { // Is a video
+        console.info('Video detected. Will use protocol: \n ');
+        return `youtu.beast:PlayVideo?ID=${YTParser.hasVideo(url)}`;
     }
 
     if (YTParser.isHomepage(url)) {
         return "youtu.beast:"
     }
-    return;
 }
 
 function getProtocolFromUrl(url, tabId) {
